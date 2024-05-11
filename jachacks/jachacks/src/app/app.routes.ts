70b92d33b634dc3e51c8282dc992bd1a027341eb
Linden Brochu <lindenb477@hotmail.com>
2024-05-11 13:01:51 -0400

    Add inputs to page

--- jachacks/jachacks/src/app/app.routes.ts
+++ jachacks/jachacks/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { PasswordCheckerComponent } from './pages/password-checker/password-checker.component';
 
 export const routes: Routes = [
-    { path: '', component: MainComponent }
+    { path: '', component: MainComponent },
+    { path: 'password-checker', component: PasswordCheckerComponent }
 ];
--- jachacks/jachacks/src/app/pages/password-checker/password-checker.component.ts
+++ jachacks/jachacks/src/app/pages/password-checker/password-checker.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-password-checker',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './password-checker.component.html',
   styleUrl: './password-checker.component.scss'
 })
 export class PasswordCheckerComponent {
+  showPassword: boolean = false;
+  password: string = '';
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  setPassword(password: string) {
+    this.password = password;
+  }
 }

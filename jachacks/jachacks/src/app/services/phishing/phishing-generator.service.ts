cb686ea2efb30f08284e67f214a45b7294227d74
Veselin Getov <veselin.getov.1@ens.etsmtl.ca>
2024-05-11 17:56:10 -0400

    Phishing Generator v1

--- jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
+++ jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ChatGPTService } from '../chatgpt/chat-gpt.service';
+import { __addDisposableResource } from 'tslib';
+import { ɵDomAdapter } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhishingGeneratorService {
 
-  constructor(private chatGPTService:ChatGPTService) { }
+  constructor(private chatGPTService:ChatGPTService) { 
+    
+  }
+
+  async sendGPTRequest(specificRequest: string) : Promise <string> {
+    let response:string = await this.chatGPTService.sendPrompt(this.getRequestTemplate(specificRequest));
+    return response;
+  }
+  
+  private getRequestTemplate(changeableValue: string) {
+    let templateRequest: string = `
+    ChatGPT Crafter Request
+    `;
+    return templateRequest;
+  }
 }

9e7f7df67c3cc37d72580dcce89b724d628fc79a
Linden Brochu <lindenb477@hotmail.com>
2024-05-11 13:01:51 -0400

    Add inputs to page

--- jachacks/jachacks/src/app/app.routes.ts
+++ jachacks/jachacks/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { PhishingDetectComponent } from './pages/phishing/detect/detect.component';
 import { PhishingCreateComponent } from './pages/phishing/create/create.component';
+import { PasswordCheckerComponent } from './pages/password-checker/password-checker.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'phishing/detect', component: PhishingDetectComponent },
   { path: 'phishing/create', component: PhishingCreateComponent },
+  { path: 'password-checker', component: PasswordCheckerComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }, // if route doesn't exist
 ];

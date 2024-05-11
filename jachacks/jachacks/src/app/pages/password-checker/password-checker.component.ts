a22a49c513fb942dd97deb770a174a7c63f18a23
Linden Brochu <lindenb477@hotmail.com>
2024-05-11 17:26:22 -0400

    Making an working flow

--- jachacks/jachacks/src/app/pages/password-checker/password-checker.component.ts
+++ jachacks/jachacks/src/app/pages/password-checker/password-checker.component.ts
@@ -2,8 +2,8 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {PasswordAnalyseState, PasswordAnalysis} from "../../types/passwordAnalyse";
 import rockYou from '../../../assets/rockyou.json';
-import translateMesure from "../../../assets/translateMesure.json";
 import {NgClass} from "@angular/common";
+import {PasswordCheckerService} from "../../services/password/password-checker.service";
 
 @Component({
   selector: 'app-password-checker',
@@ -20,11 +20,24 @@ export class PasswordCheckerComponent implements OnInit {
   password: string = '';
   analyseState: PasswordAnalyseState = PasswordAnalyseState.NotAnalyzed;
 
-  private readonly availableWidth: Array<string> = translateMesure as Array<string>;
+  private readonly availableWidth: Array<string> = [
+    "translate-x-0%",
+    "translate-x-10%",
+    "translate-x-20%",
+    "translate-x-30%",
+    "translate-x-40%",
+    "translate-x-50%",
+    "translate-x-60%",
+    "translate-x-70%",
+    "translate-x-80%",
+    "translate-x-90%"
+  ];
   public transform: string = '';
   @ViewChild('gradiant') gradiant: ElementRef<HTMLDivElement> | undefined;
 
   public analysis: PasswordAnalysis|undefined;
+
+  constructor(private passwordCheckerService: PasswordCheckerService) { }
 
   ngOnInit() {
   }
@@ -36,12 +49,8 @@ export class PasswordCheckerComponent implements OnInit {
   async changePassword() {
     if (this.password) {
       this.analyseState = PasswordAnalyseState.Analyzing;
-      this.transform = this.availableWidth[this.availableWidth.length - 1];
-      this.analysis = {
-        gptDegreeOfDanger: 9,
-        gptRecommandation: '',
-        hasRockYouMatch: this.getRockYouMatch()
-      };
+      this.analysis = await this.passwordCheckerService.checkPassword(this.password);
+      this.transform = this.availableWidth[isNaN(this.analysis.gptDegreeOfDanger) ? 0 : this.analysis.gptDegreeOfDanger];
 
       this.analyseState = PasswordAnalyseState.Analyzed;
     }

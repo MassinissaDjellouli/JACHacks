ac3db687c8014d4471373c00b20375b2b2c6cd32
Linden Brochu <lindenb477@hotmail.com>
2024-05-12 01:38:06 -0400

    Make the code analyser work

--- jachacks/jachacks/src/app/services/vuln/vuln-detector.service.ts
+++ jachacks/jachacks/src/app/services/vuln/vuln-detector.service.ts
@@ -8,8 +8,8 @@ export class VulnDetectorService {
   constructor(private chatGPTService: ChatGPTService) {}
 
   private readonly basePrompt: string =
-    'Analyze the following code for security vulnerabilities. Give me a list of all vulnerabilities with each element of the list separated with %%% and each element using the following format : ' +
-    '"VULNERABILITY: [insert vulnerability here with a short precise explanation] FIX: [insert the way to fix the vulnerability here] EXAMPLE: [provide a short and simple code example for the fix with no special formatting]. ';
+    'Analyze the following code for security vulnerabilities. Give me a list of all vulnerabilities with each element of the list and each element using the following format : ' +
+    'A description of the vulnerability, a fix and an example of code using the fix.';
   //'If your answer is longer than one paragraph, add a TL;DR at the end of your response to summarize the response.\n';
 
   async detectVulnerabilities(code: string): Promise<string> {

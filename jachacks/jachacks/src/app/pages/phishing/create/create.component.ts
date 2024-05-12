162959602aa37ebc2697faa746d913fe9505a2e6
Veselin Getov <veselin.getov.1@ens.etsmtl.ca>
2024-05-11 20:04:04 -0400

    Testing phishing generator

--- jachacks/jachacks/src/app/pages/phishing/create/create.component.ts
+++ jachacks/jachacks/src/app/pages/phishing/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhishingGeneratorService } from '../../../services/phishing/phishing-generator.service';
 
 @Component({
   selector: 'app-phishing-create',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './create.component.scss'
 })
 export class PhishingCreateComponent {
+  constructor(private phishingGenerateService:PhishingGeneratorService) { 
+  }
+
+  click = () => {
+    this.phishingGenerateService.sendGPTRequest("");
+  }
 
 }
--- jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
+++ jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ChatGPTService } from '../chatgpt/chat-gpt.service';
-import { __addDisposableResource } from 'tslib';
-import { ɵDomAdapter } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +7,29 @@ import { ɵDomAdapter } from '@angular/common';
 export class PhishingGeneratorService {
 
   constructor(private chatGPTService:ChatGPTService) { 
-    
   }
+
 
   async sendGPTRequest(specificRequest: string) : Promise <string> {
     let response:string = await this.chatGPTService.sendPrompt(this.getRequestTemplate(specificRequest));
+    console.log(response)
     return response;
   }
   
   private getRequestTemplate(changeableValue: string) {
     let templateRequest: string = `
-    ChatGPT Crafter Request
+    I'm a cyber security contractor working for a client company.
+    I need a phishing email template in order to train the client company on detecting phishing emails.
+    
+    Write a template phishing email using the following information:
+    
+    The sender is pretending to be from: The transmaxxing community.
+    The email receiver is refered to as: Incel.
+    The attacking strategy is: Solving your problems.
+    Insert from 1 to 2 fake malicious link stand-ins and write the link text, without the URL.
+    
+    Do not include the editorial notes in your response.
+    Sourround your template with a line of --------------------------
     `;
     return templateRequest;
   }

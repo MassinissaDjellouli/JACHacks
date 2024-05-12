42e2a5b1b9c75ef5088a1e49a17a2152c9fef8bc
Linden Brochu <lindenb477@hotmail.com>
2024-05-12 06:34:04 -0400

    Add spinning wheel when request

--- jachacks/jachacks/src/app/pages/phishing/create/create.component.ts
+++ jachacks/jachacks/src/app/pages/phishing/create/create.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PhishingGeneratorService } from '../../../services/phishing/phishing-generator.service';
 import { InputComponent } from '../../../components/input/input.component';
 import { FormsModule } from '@angular/forms';
+import {PhishingAnalyseState} from "../../../types/PhishingAnalyse";
 //import { cdkScrollable } from '@angular/cdk/collections';
 
 @Component({
@@ -17,30 +18,22 @@ export class PhishingCreateComponent {
   recepient: string = 'Employee';
   strategy: string = 'Suspicious activity';
   private response: string[] = []
+  phishingAnalyseState: PhishingAnalyseState = PhishingAnalyseState.NotAnalyzed;
 
-  click = async () => {
+  click = () => {
+    this.phishingAnalyseState = PhishingAnalyseState.Analyzing;
+    this.request().then(() => {
+      this.phishingAnalyseState = PhishingAnalyseState.Analyzed;
+    });
+  }
+
+  private async request() : Promise<void> {
     this.response = (await this.PhishingGeneratorService.sendGPTRequest(this.sender, this.recepient, this.strategy)).split("\n");
   }
+
   public getResponse(): string[] {
     return this.response;
   }
-}
-/* @Component({
-  selector: 'app-virtual-scroll',
-  template: `
-    <div cdkScrollable [items]="items | async">
-      <div *cdkVirtualFor="let item of items | async">
-        {{ item.name }}
-      </div>
-    </div>
-  `,
-})
-export class VirtualScrollComponent {
-  items = [];
 
-  constructor() {
-    for (let i = 0; i < 10000; i++) {
-      //this.items.push({ name: `Item ${i}` });
-    }
-  }
-} */
+  protected readonly PhishingAnalyseState = PhishingAnalyseState;
+}

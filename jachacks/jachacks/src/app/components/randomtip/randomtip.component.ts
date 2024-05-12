0146a7750dd0e76978e81174d6acf68ffcfa10dd
euphoria974 <euphoria.dev@protonmail.com>
2024-05-12 10:52:48 -0400

    last last minute

--- jachacks/jachacks/src/app/components/randomtip/randomtip.component.ts
+++ jachacks/jachacks/src/app/components/randomtip/randomtip.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { ChatGPTService } from '../../services/chatgpt/chat-gpt.service';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-randomtip',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './randomtip.component.html',
-  styleUrl: './randomtip.component.scss'
+  styleUrl: './randomtip.component.scss',
 })
 export class RandomtipComponent {
-
-  constructor(private chatGPTService:ChatGPTService) { }
+  constructor(private chatGPTService: ChatGPTService) {}
 
   private randomTip: string = '';
 
@@ -22,36 +22,45 @@ export class RandomtipComponent {
     return this.randomTip;
   }
   private activeBtn = true;
-  isActiveBtn = () => this.activeBtn; 
+  isActiveBtn = () => this.activeBtn;
   public generateRandomTip = async () => {
     this.activeBtn = false;
-    this.randomTip = (await this.chatGPTService.sendPrompt("Give me an advanced cybersecurity tip under 40 words for a " + (Math.random() > 0.5 ?"Red":"Blue") + " team cybersecurity expert about " + this.getRandomCategory(),false,1)).replace("ANSWER: ","");
+    this.randomTip = (
+      await this.chatGPTService.sendPrompt(
+        'Give me an advanced cybersecurity tip under 40 words for a ' +
+          (Math.random() > 0.5 ? 'Red' : 'Blue') +
+          ' team cybersecurity expert about ' +
+          this.getRandomCategory(),
+        false,
+        1
+      )
+    ).replace('ANSWER: ', '');
     this.activeBtn = true;
-  } 
+  };
 
-  private getRandomCategory = ():string => {
+  private getRandomCategory = (): string => {
     const categories = [
-      "Phishing",
-      "Password Security",
-      "Network Security",
-      "Data Protection",
-      "Social Engineering",
-      "Malware",
-      "Firewalls",
-      "Encryption",
-      "Binary Exploits",
-      "Linux",
-      "Windows Security",
-      "Mobile Security",
-      "Forensics",
-      "Reverse Engineering",
-      "Phisyxal Security",
-      "Ransomware",
-      "Zero-Day Attacks",
-      "DDoS Attacks",
-      "SQL Injection",
-      "Cross-Site Scripting",
-    ]
+      'Phishing',
+      'Password Security',
+      'Network Security',
+      'Data Protection',
+      'Social Engineering',
+      'Malware',
+      'Firewalls',
+      'Encryption',
+      'Binary Exploits',
+      'Linux',
+      'Windows Security',
+      'Mobile Security',
+      'Forensics',
+      'Reverse Engineering',
+      'Phisyxal Security',
+      'Ransomware',
+      'Zero-Day Attacks',
+      'DDoS Attacks',
+      'SQL Injection',
+      'Cross-Site Scripting',
+    ];
     return categories[Math.floor(Math.random() * categories.length)];
-  }
+  };
 }
--- jachacks/jachacks/src/app/pages/report-generator/report-generator.component.ts
+++ jachacks/jachacks/src/app/pages/report-generator/report-generator.component.ts
@@ -4,36 +4,42 @@ import { ReportGenerateState } from '../../types/ReportGenerate';
 import { PhishingGeneratorService } from '../../services/phishing/phishing-generator.service';
 import { InputComponent } from '../../components/input/input.component';
 import { ReportGeneratorService } from '../../services/reportGenerator/report.service';
-import {MarkdownComponent} from "ngx-markdown";
+import { MarkdownComponent } from 'ngx-markdown';
+import { ButtonComponent } from '../../components/button/button.component';
 @Component({
   selector: 'app-report-generator',
   standalone: true,
-  imports: [InputComponent, FormsModule,MarkdownComponent],
+  imports: [InputComponent, FormsModule, MarkdownComponent, ButtonComponent],
   templateUrl: './report-generator.component.html',
-  styleUrl: './report-generator.component.scss'
+  styleUrl: './report-generator.component.scss',
 })
 export class ReportGeneratorComponent {
-  constructor(private reportGeneratorService:ReportGeneratorService) { }
+  constructor(private reportGeneratorService: ReportGeneratorService) {}
   reportType: string = 'incident';
-  content: string = 'We got hacked 4 times, and we lost 2 clients because of it.';
+  content: string =
+    'We got hacked 4 times, and we lost 2 clients because of it.';
   twist: string = 'positive';
-  private response: string = ""
+  private response: string = '';
   reportGenerateState: ReportGenerateState = ReportGenerateState.NotGenerated;
 
   click = () => {
     this.reportGenerateState = ReportGenerateState.Generating;
     console.log(this.reportGenerateState);
-    
+
     this.request().then(() => {
       this.reportGenerateState = ReportGenerateState.Generated;
     });
-  }
+  };
 
-  private async request() : Promise<void> {
-    this.response = (await this.reportGeneratorService.sendGPTRequest(this.reportType, this.content, this.twist))
-    if(this.response.startsWith("```"))
+  private async request(): Promise<void> {
+    this.response = await this.reportGeneratorService.sendGPTRequest(
+      this.reportType,
+      this.content,
+      this.twist
+    );
+    if (this.response.startsWith('```'))
       this.response = this.response.substring(3, this.response.length - 3);
-    if(this.response.endsWith("```"))
+    if (this.response.endsWith('```'))
       this.response = this.response.substring(0, this.response.length - 3);
   }
 

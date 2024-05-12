3ff310de29d05451e167c306eeb5f9bf74a95dda
Veselin Getov <veselin.getov.1@ens.etsmtl.ca>
2024-05-11 20:04:04 -0400

    Testing phishing generator

--- jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
+++ jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ChatGPTService } from '../chatgpt/chat-gpt.service';
-import { __addDisposableResource } from 'tslib';
-import { ɵDomAdapter } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'

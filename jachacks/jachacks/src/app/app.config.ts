b833ed725d115ddbf19d6baa95640b2f7773f788
Linden Brochu <lindenb477@hotmail.com>
2024-05-12 01:38:06 -0400

    Make the code analyser work

--- jachacks/jachacks/src/app/app.config.ts
+++ jachacks/jachacks/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {MARKED_OPTIONS, provideMarkdown} from "ngx-markdown";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideMarkdown({
+
+    })
+  ]
 };

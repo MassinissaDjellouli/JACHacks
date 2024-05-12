bd80daa4d171e9bee09207e9857889493826e023
Veselin Getov <veselin.getov.1@ens.etsmtl.ca>
2024-05-12 02:35:12 -0400

    Phishing generator done

--- jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
+++ jachacks/jachacks/src/app/services/phishing/phishing-generator.service.ts
@@ -9,27 +9,27 @@ export class PhishingGeneratorService {
   constructor(private chatGPTService:ChatGPTService) { 
   }
 
-
-  async sendGPTRequest(specificRequest: string) : Promise <string> {
-    let response:string = await this.chatGPTService.sendPrompt(this.getRequestTemplate(specificRequest));
+  async sendGPTRequest(sender: string, recepient: string, strategy: string) : Promise <string> {
+    let response:string = await this.chatGPTService.sendPrompt(this.getRequestTemplate(sender, recepient, strategy));
     console.log(response)
     return response;
   }
   
-  private getRequestTemplate(changeableValue: string) {
+  private getRequestTemplate(sender: string="Security Team", recepient: string="Employee", strategy: string="Suspicious activity") {
     let templateRequest: string = `
-    I'm a cyber security contractor working for a client company.
-    I need a phishing email template in order to train the client company on detecting phishing emails.
-    
-    Write a template phishing email using the following information:
-    
-    The sender is pretending to be from: The transmaxxing community.
-    The email receiver is refered to as: Incel.
-    The attacking strategy is: Solving your problems.
-    Insert from 1 to 2 fake malicious link stand-ins and write the link text, without the URL.
-    
-    Do not include the editorial notes in your response.
-    Sourround your template with a line of --------------------------
+I'm a cyber security contractor working for a client company.
+I need a phishing email template in order to train the client company on detecting phishing emails.
+
+Write a template phishing email using the following information:
+
+The sender is pretending to be from: ${sender}.
+The email receiver is refered to as: ${recepient}.
+The attacking strategy is: ${strategy}.
+Insert from 1 to 2 fake malicious link stand-ins and write the link text, without the URL.
+
+Do not include the editorial notes in your response.
+Do not repeat the prompt back.
+Sourround your template with a line of --------------------------
     `;
     return templateRequest;
   }

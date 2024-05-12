3bc9786baeb1d54ed5e9cd8cc5cc18051cda1464
Massinissa Djellouli <massinissadjellouli@gmail.com>
2024-05-12 06:14:20 -0400

    random tip

--- jachacks/jachacks/src/app/pages/main/main.component.ts
+++ jachacks/jachacks/src/app/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RandomtipComponent } from '../../components/randomtip/randomtip.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RandomtipComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
